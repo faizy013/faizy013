605cde89661ddc20e2f10f5699c6117e2777b1c5
Muhammad Umar Farooq <150376779+faizy013@users.noreply.github.com>
2026-08-27 11:44:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -384,7 +384,7 @@ if __name__ == '__main__':
     formatter('account data', user_time)
 
     # >>> STEP 1: SET YOUR OWN BIRTH DATE HERE (year, month, day) <<<
-    age_data, age_time = perf_counter(daily_readme, datetime.datetime(2000, 1, 1))
+    age_data, age_time = perf_counter(daily_readme, datetime.datetime(2004, 10, 13))
     formatter('age calculation', age_time)
 
     total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
